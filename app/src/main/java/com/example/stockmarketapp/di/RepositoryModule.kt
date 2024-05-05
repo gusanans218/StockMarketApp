67a1bbba8304b013c6dc4f74859dd7919565af50
SeungHyun <qustmdgus1212@gmail.com>
2024-05-05 23:12:02 +0900

    RepositoryModule 개발

---
+++ app/src/main/java/com/example/stockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.example.stockmarketapp.di
+
+import com.example.stockmarketapp.data.csv.CSVParser
+import com.example.stockmarketapp.data.csv.CompanyListingParser
+import com.example.stockmarketapp.data.repository.StockRepository
+import com.example.stockmarketapp.data.repository.StockRepositoryImpl
+import com.example.stockmarketapp.domain.model.CompanyListing
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingParser: CompanyListingParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
