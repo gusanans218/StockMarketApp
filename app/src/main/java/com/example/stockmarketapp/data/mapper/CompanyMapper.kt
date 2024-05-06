9ff83b8715dd103c8e5f6ff4b2669c1b1bcc7c02
SeungHyun <qustmdgus1212@gmail.com>
2024-05-06 16:35:09 +0900

    회사 정보 관련 Mapper에 추가

--- app/src/main/java/com/example/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/example/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package com.example.stockmarketapp.data.mapper
 
 import com.example.stockmarketapp.data.local.CompanyListingEntity
+import com.example.stockmarketapp.data.remote.dto.CompanyInfoDto
+import com.example.stockmarketapp.domain.model.CompanyInfo
 import com.example.stockmarketapp.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing(): CompanyListing {
@@ -16,5 +18,15 @@ fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
         name = name,
         symbol = symbol,
         exchange = exchange
+    )
+}
+
+fun CompanyInfoDto.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: ""
     )
 }
